437cd536008931a5ada68655fe0c6b4097b3f024
Zachary Winne <zach.winne@gmail.com>
2023-03-15 08:00:13 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded( {extended: true} ));
+
+const ejs = require('ejs');
+app.set('view engine', 'ejs');
+
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/userDB');
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.route('/login')
+  .get( (req, res) => {
+    res.render('login')
+  })
+
+  .post( (req, res) => {
+    User.findOne({
+      email: req.body.username
+    })
+    .then( (foundUser) => {
+      if (foundUser.password === req.body.password) {
+        res.render('secrets');
+      }
+      else {
+        res.redirect('/login')
+      }
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  });
+
+app.route('/register')
+
+  .get( (req, res) => {
+    res.render('register')
+  })
+
+  .post( (req, res) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: req.body.password
+    });
+
+    newUser.save()
+    .then( () => {
+      res.render('secrets');
+    })
+    .catch( (err) => {
+      console.log(err);
+    });
+  });
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port ' + (process.env.PORT || 3000) + '...');
+});
